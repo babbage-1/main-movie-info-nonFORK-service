0df817d0aa3737b2903f42744355bf1260fa1332
ahchung23 <hahneol@gmail.com>
2019-04-24 03:18:52 -0400

    add folder to generate 10M data and 1M sample data
    dataGenerator folder houses files to generate sdc-cassandra-data.csv &
    sdc-posgresql-data.csv
    
    dataSampleGenerator folder houses files to generate
    sdc-sample-cassandra-data.csv & sdc-sample-postgresql-data.csv

---
+++ dataSampleGenerator/s3SampleImages.js
@@ -0,0 +1,32 @@
+const AWS = require('aws-sdk');
+AWS.config.loadFromPath('s3Config.json');
+const s3 = new AWS.S3();
+
+const params = {
+  Bucket: 'sdc-andrew-movie-posters',
+  MaxKeys: 3,
+};
+
+const listAllObjects = async () => {
+  const imgUrlArray = [];
+  try {
+    const data = await s3.listObjectsV2(params).promise();
+    data.Contents.forEach((elem) => {
+      let key = elem.Key;
+      for (let i = 0; i < key.length; i += 1) {
+        if (key[i] === ' ') {
+          key = `${key.slice(0, i)}+${key.slice(i + 1)}`;
+        }
+      }
+      const url = `https://s3.us-east-2.amazonaws.com/sdc-andrew-movie-posters/${key}`;
+      imgUrlArray.push(url);
+    });
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+  return imgUrlArray;
+};
+
+listAllObjects();
+module.exports = listAllObjects;
